ff9e586d706320f07ca7e3159abe8096b2dd8040
Debomita Chakraborty <42289902+Debomita02@users.noreply.github.com>
2019-04-02 17:30:50 +0530

    Update to create_graph.py
    Upload human PPI interaction file in .csv format. 
    Upload another .csv file with the list of human proteins obtained from host-viral interaction network. 
    The program would filter human PPI data that have combined_score >= 900. It would create a subgraph of the human PPI network using the nodes (human proteins) from the host-viral interaction network such that only those neighbours that have a path distance of upto 3 would be present in the subgraph.

--- create_graph.py
+++ create_graph.py
@@ -6,6 +6,7 @@ Created on Mon Apr  1 10:58:47 2019
 @author: debomita
 """
 
+import sys
 import pandas as pd
 import networkx as nx
 from tkinter import Tk
@@ -13,25 +14,57 @@ from tkinter import filedialog
 from tkinter.filedialog import askopenfilename
 
 Tk().withdraw()
+# Upload file with human PPI data
 source_file = askopenfilename()
 print(source_file)
+# Upload file with list of file names containing human proteins
+# from viral-human interaction network
+subgraph_file = askopenfilename()
+print(subgraph_file)
 
-file = pd.read_csv(source_file, sep = "\t")
-file = file[file["combined_score"]>=200]
 
+file = pd.read_csv(source_file, sep = " ")
+# Filter out PPIs with combined_score >= 900
+file = file[file["combined_score"]>=900]
+# Initialize list for holding node data of human PPI graph
 nodelist = []
 
+# Get proteins from columns 1 and 2 of human PPI file into the node list
 for col in ['protein1', 'protein2']:
+    # Keep only unique nodes from the columns
     nodelist.extend(file[col].unique().tolist())
 
-nodelist = set(nodelist)
-
+# Get edge list from the file
 edgelist = [(row['protein1'], row['protein2']) for index, row in file.iterrows()]
 
 
+# Create graph for the human PPI network
 G = nx.Graph()
 G.add_nodes_from(nodelist)
 G.add_edges_from(edgelist)
     
-    
+# Print the human PPI network info    
 print(nx.info(G))
+
+# Read file containing file names (<virus>_human.csv)
+sub_fnames = pd.read_csv(subgraph_file)
+sub_fnames1 = [row['fname'] for index, row in sub_fnames.iterrows()]
+for sub_line in sub_fnames1:  
+
+    # Get node list from column 1 of file
+   sub_node_list = pd.read_csv(sub_line, usecols=[0])
+   sub_node_list1 = [row['prot_id'] for index, row in sub_node_list.iterrows()]
+    # Initialize subgraph
+   G_sub = nx.Graph() 
+   for node_i in sub_node_list1:
+       try:
+           # Pass each node to get a subgraph of upto the 3rd nearest neighbour
+           G_sub_i = nx.ego_graph(G, node_i,radius = 3, center=True, undirected=True)
+           pass
+       except:
+           print(sys.exc_info())
+           pass
+       # Take union of the subgraphs generated for each node in the file              
+       G_sub = nx.compose(G_sub,G_sub_i)
+   # Print the info for subgraphs of each file
+   print("Subgraph_"+sub_line[:-4],nx.info(G_sub))
