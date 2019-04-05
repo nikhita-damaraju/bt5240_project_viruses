4d64a5d179af69ca703912cf8fcc156be33f626b
Debomita Chakraborty <42289902+Debomita02@users.noreply.github.com>
2019-04-05 17:17:16 +0530

    Update create_graph.py
    The <virus>_human.csv files listed in gen_file_list.csv should be saved in the 'Home' folder. A log file with the name 'subgraph_except.log' will be created/updated with the exceptions that happen during subgraph generation.

--- create_graph.py
+++ create_graph.py
@@ -2,24 +2,31 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr  1 10:58:47 2019
+The <virus>_human.csv files listed in gen_file_list.csv
+should be saved in the 'Home' folder.
+A log file with the name 'subgraph_except.log' will be
+created/updated with the exceptions that happen during
+subgraph generation.
 
 @author: debomita
+
 """
 
 import sys
 import pandas as pd
 import networkx as nx
 from tkinter import Tk
-from tkinter import filedialog
+#from tkinter import filedialog
 from tkinter.filedialog import askopenfilename
+import logging
 
 Tk().withdraw()
 # Upload file with human PPI data
-source_file = askopenfilename()
+source_file = askopenfilename(title = 'Upload human PPI file',filetypes = (("csv files","*.csv"),("all files","*.*")))
 print(source_file)
 # Upload file with list of file names containing human proteins
 # from viral-human interaction network
-subgraph_file = askopenfilename()
+subgraph_file = askopenfilename(title = 'Upload "gen_file_list.csv"',filetypes = (("csv files","*.csv"),("all files","*.*")))
 print(subgraph_file)
 
 
@@ -46,6 +53,8 @@ G.add_edges_from(edgelist)
 # Print the human PPI network info    
 print(nx.info(G))
 
+#********************Constructing the subgraph*********************************
+
 # Read file containing file names (<virus>_human.csv)
 sub_fnames = pd.read_csv(subgraph_file)
 sub_fnames1 = [row['fname'] for index, row in sub_fnames.iterrows()]
@@ -62,7 +71,10 @@ for sub_line in sub_fnames1:
            G_sub_i = nx.ego_graph(G, node_i,radius = 3, center=True, undirected=True)
            pass
        except:
-           print(sys.exc_info())
+           
+           logging.basicConfig(filename='subgraph_except.log',level=logging.DEBUG)
+           logging.debug(sys.exc_info())
+           logging.info('Filename:'+sub_line+'\n')
            pass
        # Take union of the subgraphs generated for each node in the file              
        G_sub = nx.compose(G_sub,G_sub_i)
